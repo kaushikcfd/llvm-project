787fb05aaa9e87e6035db4465dd760fa0e69071e
Kaushik Kulkarni <kgk2@illinois.edu>
2020-04-28 17:32:37 -0500

    adds pretty printer (compiles correctly again)

--- mlir/lib/Transforms/CS526ArrayDependenceAnalysis.cpp
+++ mlir/lib/Transforms/CS526ArrayDependenceAnalysis.cpp
@@ -23,26 +23,122 @@
 
 using namespace mlir;
 
+// TODO: Add a statistic to report how many pairs were analyzed.
 
 namespace {
 
 
 template <typename T>
-using Matrix = vector<vector<T>>;
+using Matrix = std::vector<std::vector<T>>;
+
+struct CS526ArrayDependenceAnalysis : public PassWrapper<CS526ArrayDependenceAnalysis, FunctionPass> {
+  void runOnFunction() override;
+};
+} // end anonymous namespace
+
 
 
 template <typename T>
 static Matrix<T> createMatrix(size_t N) {
-  Matrix<T> result(N, vector<T>(N));
+  Matrix<T> result(N, std::vector<T>(N));
   return result;
 }
 
-
-
-
-static void printMatrix(Matrix<short> R, vector<Op> affineLoadsStores) {
+static void printMatrix(Matrix<short> R, std::vector<Operation*> affineLoadsStores) {
   // @TODO: Assign names to rows columsn
-  return;
+  int N = affineLoadsStores.size();
+
+  const int cellWidth = 6;
+  auto formatToCell = [cellWidth](std::string s) {
+    int diff = cellWidth - s.size();
+    assert((diff >= 0) && "cellWidth not suitable");
+    std::string bigPad(diff/2 + diff%2, ' ');
+    std::string smallPad(diff/2, ' ');
+    if (cellWidth % 2)
+      return smallPad + s + bigPad;
+    else
+      return bigPad + s + smallPad;
+  };
+
+  auto accessName = [](Operation* Op) {
+    std::string result;
+    if (isa<AffineLoadOp>(Op))
+      result = "L";
+    else if (isa<AffineStoreOp>(Op))
+      result = "S";
+    else
+      assert(false && "Received a non-memory op!");
+
+    if (auto fileLineCol = (Op->getLoc()).dyn_cast<FileLineColLoc>())
+      result += std::to_string(fileLineCol.getLine());
+    else
+      result += "xx";
+
+    return result;
+  };
+
+  // {{{ table begin (separator)
+  
+  llvm::dbgs() << "+";
+  for (int i = -1; i < N; ++i) {
+    llvm::dbgs() << formatToCell(std::string(cellWidth, '-'));
+    llvm::dbgs() << "+";
+  }
+  llvm::dbgs() << "\n";
+
+  // }}}
+
+  // {{{ first header row
+
+  llvm::dbgs() << "|";
+  llvm::dbgs() << formatToCell("");
+  llvm::dbgs() << "|";
+
+  for (Operation* Op : affineLoadsStores) {
+    llvm::dbgs() << formatToCell(accessName(Op));
+    llvm::dbgs() << "|";
+  }
+  llvm::dbgs() << "\n";
+
+  // }}}
+  
+  // {{{ second row (separator)
+  
+  llvm::dbgs() << "+";
+  for (int i = -1; i < N; ++i) {
+    llvm::dbgs() << formatToCell(std::string(cellWidth, '-'));
+    llvm::dbgs() << "+";
+  }
+  llvm::dbgs() << "\n";
+
+  // }}}
+
+  // {{{ Print the matrix
+  
+  for (int i = 0; i < N; ++i)
+  {
+    llvm::dbgs() << "|";
+    llvm::dbgs() << formatToCell(accessName(affineLoadsStores[i]));
+    llvm::dbgs() << "|";
+    for (int j = 0; j < N; ++j) {
+      llvm::dbgs() << formatToCell(std::to_string(R[i][j]));
+      llvm::dbgs() << "|";
+    }
+    llvm::dbgs() << "\n";
+  }
+
+  // }}}
+
+  // {{{ last row (separator)
+  
+  llvm::dbgs() << "+";
+  for (int i = -1; i < N; ++i) {
+    llvm::dbgs() << formatToCell(std::string(cellWidth, '-'));
+    llvm::dbgs() << "+";
+  }
+  llvm::dbgs() << "\n";
+
+  // }}}
 }
 
 
@@ -59,44 +155,36 @@ static void printMatrix(Matrix<short> R, vector<Op> affineLoadsStores) {
  * Rij =  0    if we are unable to ascertain the above 2 conditions on the
  *             memory accessed by Opi, Opj
  */
-void printZIVResults(vector<Op> affineLoadsStores) {
-  auto R = createMatrix<short>(affineLoadsStores.size());
-  printMatrix(R);
+void printZIVResults(std::vector<Operation*> affineLoadsStores) {
+  int N = affineLoadsStores.size();
+  auto R = createMatrix<short>(N);
+  // TODO: compute the dependencies here...
+  printMatrix(R, affineLoadsStores);
 }
 
 
+void CS526ArrayDependenceAnalysis::runOnFunction() {
+  FuncOp func = getFunction();
+  llvm::dbgs() << "================================================================\n";
+  llvm::dbgs() << "And the function of interest is -- \n";
+  func.print(llvm::dbgs());
+  llvm::dbgs() << "\n";
+  llvm::dbgs() << "================================================================\n";
 
-struct CS526ArrayDependenceAnalysis : public PassWrapper<CS526ArrayDependenceAnalysis, FunctionPass> {
-  void runOnFunction() override {
+  std::vector<Operation*> affineLoadsStores;
 
-    FuncOp func = getFunction();
-    llvm::dbgs() << "================================================================\n";
-    llvm::dbgs() << "And the function of interest is -- \n";
-    func.print(llvm::dbgs());
-    llvm::dbgs() << "\n";
-    llvm::dbgs() << "================================================================\n";
+  // populate affineLoadsStores
+  func.walk([&](Operation *op) {
+    if (isa<AffineLoadOp>(op) || isa<AffineStoreOp>(op)) {
+      affineLoadsStores.push_back(op);
+    }
+  });
 
-    llvm::dbgs() << "Printing all the loads and stores in the functions:\n";
+  llvm::dbgs() << "Pretty printing out table:\n";
+  printZIVResults(affineLoadsStores);
+  llvm::dbgs() << "================================================================\n";
+}
 
-    func.walk([&](Operation *op) {
-      if (isa<AffineLoadOp>(op) || isa<AffineStoreOp>(op)) {
-        op->print(llvm::dbgs());
-        Location loc = op->getLoc();
-        if (auto fileLineCol = loc.dyn_cast<FileLineColLoc>()) {
-          llvm::dbgs() << " ; On line " << fileLineCol.getLine();
-        }
-        else {
-          lvm::dbgs() << "Unkown loc.";
-        }
-        llvm::dbgs() << "\n";
-      }
-    });
-    llvm::dbgs() << "================================================================\n";
-
-  }
-
-};
-} // end anonymous namespace
 
 // Register this pass to make it accessible to utilities like mlir-opt.
 // (Pass registration is discussed more below)
